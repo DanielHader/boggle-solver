0299eafea19c83296799be6f7c5f97b2afe3e983
Daniel Hader <dhader96@gmail.com>
2025-06-24 21:55:48 -0500

    load from dictionary file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,35 @@ mod boggle;
 
 use boggle::Board;
 
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use std::env;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path> + std::fmt::Debug, {
+    println!("{:?}", filename);
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 fn main() {
 
     let dict_file = env::var("DICTIONARY_FILE").unwrap();
-    println!("{:?}", dict_file);
+    match read_lines(&dict_file) {
+        Ok(lines) => {
+            let mut i: usize = 0;
+            for line in lines.map_while(Result::ok) {
+                println!("{}", line);
+                
+                i += 1;
+                if i == 10 { break; }
+            }
+        },
+        Err(_) => {
+            println!("unable to load file {}", &dict_file);
+        }
+    }
 
     let cubes = vec![
         "E", "G", "T", "E",
